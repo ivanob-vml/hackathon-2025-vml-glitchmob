4b9314b4561213ad271daefd0100d28a1fbc1a38
Kishan Bhanderi <kbhanderi@wundermanthompson.com>
2025-01-16 17:55:02 +0000

    Latest UI changes

--- src/components/PromptStart.tsx
+++ src/components/PromptStart.tsx
@@ -12,19 +12,21 @@ const PromptStart: React.FC<PromptStartProps> = ({ onPromptClick }) => {
   return (
       <div id="prompts" className="flex flex-wrap justify-start items-center flex-row gap-2 px-4 animate-fadeOut">
       <p className="flex flex-col text-[#000000] font-['Figtree']">Select a prompt:</p><br/>
-      {promptStrings.map((prompt, index) => (
-        <div
-          key={index}
-          className="flex justify-center items-center flex-row gap-2.5 py-2 px-3 bg-[#F4F4F4] border-solid border-[#4927AF] border rounded-lg w-[167px]"
-          style={{width: '167px'}}>
-            <p className="flex-1 text-[#000000] font-['Figtree']" onClick={() => {
-            onPromptClick(prompt);
-            document.querySelector('div[id="prompts"]')?.classList.add('hidden');
-            }}>
-            {prompt}
-            </p>
-        </div>
-      ))}
+      <div className="flex gap-2.5 flex-wrap">
+        {promptStrings.map((prompt, index) => (
+          <div
+            key={index}
+            className="flex-1 justify-center items-center flex-row gap-2.5 py-2 px-3 bg-[#F4F4F4] border-solid border-[#4927AF] border rounded-lg w-[167px]"
+            style={{width: '167px'}}>
+              <p className="flex p-1 text-[#000000] items-center font-['Figtree']" onClick={() => {
+              onPromptClick(prompt);
+              document.querySelector('div[id="prompts"]')?.classList.add('hidden');
+              }}>
+              {prompt}
+              </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
